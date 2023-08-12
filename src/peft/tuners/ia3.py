f719282c7dd8ba1ffa03f8aaa37d73aedd03f761
Seungyoo-Lee <86828497+dopeornope-Lee@users.noreply.github.com>
2023-08-13 00:53:20 +0900

    Update ia3.py

--- src/peft/tuners/ia3.py
+++ src/peft/tuners/ia3.py
@@ -505,5 +505,6 @@ if is_bnb_available():
                     if self.is_feedforward:
                         result = super().forward(x * self.ia3_l[self.active_adapter].flatten())
                     else:
+                        result = super().forward(x)
                         result = result * self.ia3_l[self.active_adapter].flatten()
             return result
